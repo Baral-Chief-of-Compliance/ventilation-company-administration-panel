9240c27877871aa3d8876d746d3fd58e76fd9f9a
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-02-03 17:36:07 +0300

    add api for brigade

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from config import Config
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,43 @@
-from app import app
+from app import app, jsonify, request
 from app.stored_procedure import call_stored_procedure
 
 
-@app.route('/')
+@app.route('/admin_panel/api/v1.0/all_brigades', methods=['GET'])
+def all_brigades():
+    if request.method == 'GET':
+        json_brigades = []
+        brigades = call_stored_procedure('all_brigades', commit=False, fetchall=True)
+
+        for br in brigades:
+            json_brigades.append({'id': br[0], 'name': br[1]})
+
+        return jsonify(json_brigades)
+
+
+@app.route('/admin_panel/api/v1.0/create_brigades', methods=['POST'])
+def create_brigades():
+    if request.method == 'POST':
+        name = request.args['name']
+        call_stored_procedure('add_brigade', [name], commit=True, fetchall=False)
+        return jsonify(f'brigade {name} is add')
+
+
+@app.route('/admin_panel/api/v1.0/inf_about_brigade', methods=['GET'])
+def inf_about_brigade():
+    if request.method == 'GET':
+        id = request.args['id']
+        inf = call_stored_procedure('inf_about_brigade', [id], commit=False, fetchall=False)
+        return jsonify({'name': inf[0]})
+
+
+@app.route('/admin_panel/api/v1.0/delete_brigade', methods=['POST'])
+def delete_brigade():
+    if request.method == 'POST':
+        id = request.args['id']
+        call_stored_procedure('delete_brigade', [id], commit=True, fetchall=False)
+        return jsonify(f'brigade id = {id} is delete')
+
+
 @app.route('/index')
 def index():
     return 'Пошел нахуй'

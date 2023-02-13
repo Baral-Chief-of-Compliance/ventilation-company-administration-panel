5a58a51fcaef599345c27888780dd9511a28eef1
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-02-13 13:34:05 +0300

    add template for add brigades and list of brigades

--- frontend/test/src/components/brigades/AddBrigades.vue
+++ frontend/test/src/components/brigades/AddBrigades.vue
@@ -1,56 +1,98 @@
 <template>
-    <div class="text-h3 py-6">Добавить бригаду</div>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+
+        <div class="text-h3 py-6 mx-10 text-left">Бригады</div>
+
+        <v-container class="d-flex justify-center">
+          <v-col>
+              <v-row class="mx-10">
+                <v-col 
+                  v-for="el in allBrigades"
+                  :key="el.id"
+                  cols="12"
+                  md="3"
+                  v-bind="props_hover"  
+                >
+                <v-hover v-slot="{ isHovering, props }">
+                  <v-card v-bind="props" height="200" width="300" class="texte-center my-8 mx-lg-auto pa-md-15" :color="isHovering ? 'indigo' : undefined">
+                    <div class="text-h6">{{ el.name }}</div>
+                  </v-card>
+                </v-hover>
+                </v-col>
+              </v-row>
+              <v-row class="mx-14 mt-15">
+                <v-btn block
+                  color="indigo"
+                  v-bind="props"
+                  >
+                  Добавить бригаду
+                </v-btn>
+              </v-row>
+          </v-col>
 
 
-    
-    
-    <v-form v-model="valid">
-    <v-container class="d-flex justify-center">
-        <v-col>
+        </v-container>
 
-        <v-row>
-                <div></div>
-                <v-text-field
-                v-model="brigades"
-                label="Название бригады"
-                color="indigo"
-                required
-            ></v-text-field>
-        </v-row>
+      </template>
 
-        <v-row>
-
-
+      <v-card>
+        <v-card-text>
+          Форма для добавления бригады
+        </v-card-text>
+        <v-form>
           <v-text-field
-            v-model="firstname"
-            label="First name"
-            required
-          ></v-text-field>
+            v-model="brigades_name"
+            label="название бригады"
+            color="indigo"
+            class="mx-5 mb-5"
+          >
 
-        
-
-    
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
-
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-    </v-row>
-</v-col>
-    </v-container>
-  </v-form>
-
+          </v-text-field>
+        </v-form>
+        <v-card-actions>
+          <v-btn color="red"  @click="dialog = false">Закрыть</v-btn>
+          <v-btn color="green"  @click="create_brigade">Добавить</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 
+
+  export default {
+    data () {
+      return {
+        dialog: false,
+        allBrigades: null,
+        brigades_name: null
+      }
+    },
+    mounted(){
+      axios.get('http://127.0.0.1:5000/admin_panel/api/v1.0/brigades')
+      .then(response => (this.allBrigades = response.data))
+    },
+    methods: {
+      create_brigade(){
+        axios.post('http://127.0.0.1:5000/admin_panel/api/v1.0/create_brigades',{
+          name: this.brigades_name
+        })
+
+        this.brigades_name = null
+        this.dialog = false
+
+        }
+    },
+    updated(){
+      axios.get('http://127.0.0.1:5000/admin_panel/api/v1.0/brigades')
+      .then(response => (this.allBrigades = response.data))
+    }
+  }
 </script>

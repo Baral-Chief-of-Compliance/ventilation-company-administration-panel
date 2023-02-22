3e19c65ba523a477fc4b0aaea6deb4d099e64301
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-02-22 16:57:52 +0300

    add template for login page

--- frontend/test/src/views/login/Login.vue
+++ frontend/test/src/views/login/Login.vue
@@ -3,8 +3,31 @@
     <v-app :style="{ background: '#3F51B5' }" >
         <div class="d-flex justify-center">
 
-            <v-card width="500" height="300" class="mt-15">
-                s
+            <v-card width="500" height="300" class="mt-15 pa-6">
+                <v-card-title>
+                    Административная панель
+                </v-card-title>
+                <v-card-subtitle>
+                    ООО "Вентелятор"
+                </v-card-subtitle>
+                <v-card-item>
+                    <v-text-field
+                        v-model="login"
+                        label="Логин"
+                        color="indigo"
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        v-model="password"
+                        label="Пароль"
+                        color="indigo"
+                    >
+                    </v-text-field>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green">Войти</v-btn>
+                    </v-card-actions>
+                </v-card-item>
             </v-card>
 
         </div>
@@ -12,3 +35,14 @@
     </v-app>
 
 </template>
+
+<script>
+    export default{
+        data(){
+            return{
+                login: null,
+                password: null
+            }
+        }
+    }
+</script>
